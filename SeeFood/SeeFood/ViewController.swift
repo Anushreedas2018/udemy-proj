927ca4a2e8fcb172934bdfb71243505aba2896f2
Anushree Das <anushreedas@anu-mbp.local>
2020-07-27 11:32:37 +0530

    Import Image Recognition model and enable camera or photo library

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Anushree Das on 25/07/20.
+//  Copyright © 2020 Anushree Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
